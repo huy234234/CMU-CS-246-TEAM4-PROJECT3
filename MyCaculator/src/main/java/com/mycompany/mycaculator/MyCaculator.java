ce3f6bd6a7a9cdf279097b13f7e360d43fc2b91d
ngocthien1504 <162582872+ngocthien1504@users.noreply.github.com>
2024-03-14 20:18:34 +0700

    Add files via upload

---
+++ MyCaculator/src/main/java/com/mycompany/mycaculator/MyCaculator.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mycaculator;
+
+/**
+ *
+ * @author testy
+ */
+public class MyCaculator {
+
+    public static void main(String[] args) {
+         mycar obj = new mycar();
+        obj.setVisible(true);
+    }
+}
